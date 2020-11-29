4cc6b07a4d2924ddb3181763f4c9496a7f42fa40
erich <eriche864@gmail.com>
2020-11-29 01:08:23 -0500

    update vets controller and index closes #15

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,14 +1,25 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index"})
-    public String listVets() {
-       return "vets/index";
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
+        return "vets/index";
     }
 
 }
