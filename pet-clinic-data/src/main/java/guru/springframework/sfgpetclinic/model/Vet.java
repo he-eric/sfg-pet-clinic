78d7e8616ea00017c5bfc7eeb82d0146fb919a60
erich <eriche864@gmail.com>
2020-11-30 22:57:29 -0500

    Add vet speciality entity closes #24

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,7 +1,16 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
 
+    private Set<VetSpeciality> specialities;
 
+    public Set<VetSpeciality> getSpecialities() {
+        return specialities;
+    }
 
+    public void setSpecialities(Set<VetSpeciality> specialities) {
+        this.specialities = specialities;
+    }
 }
