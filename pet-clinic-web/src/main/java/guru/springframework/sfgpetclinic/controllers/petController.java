b05c77dacd51ba1d4841a606a1d53f307f692e1d
erich <eriche864@gmail.com>
2021-02-03 19:56:03 -0500

    refactor pet type builder

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/petController.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/owners")
+public class petController {
+
+
+
+}
